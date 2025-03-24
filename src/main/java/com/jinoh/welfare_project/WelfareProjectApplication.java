37200373a425c52ecafd3de3457ad8b79dd25416
jinoh <jino7485@nate.com>
2025-03-24 22:55:17 +0900

    first commit

---
+++ src/main/java/com/jinoh/welfare_project/WelfareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jinoh.welfare_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelfareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelfareProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinoh/welfare_project/WelfareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinoh.welfare_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelfareProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
